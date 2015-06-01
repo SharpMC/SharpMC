c6ef54902ad2b5673b04468ade3058cca36fef94
kennyvv <kennyvvulpen@gmail.com>
2015-06-01 12:48:33 +0100

    Oops

---
+++ SharpMC/Enums/GameStateReason.cs
@@ -0,0 +1,16 @@
+﻿namespace SharpMC.Enums
+{
+	public enum GameStateReason
+	{
+		InvalidBed = 0,
+		EndRaining = 1,
+		BeginRaining = 2,
+		ChangeGameMode = 3,
+		EnterCredits = 4,
+		DemoMessage = 5,
+		ArrowHittingPlayer = 6,
+		FadeValue = 7,
+		FadeTime = 8,
+		PlayMobAppearance = 9
+	}
+}
