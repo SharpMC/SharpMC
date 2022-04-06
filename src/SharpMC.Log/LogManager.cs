db272c27e680ec4bbed3a5608a06fd8e408b2ba9
xafero <georg.berendt@outlook.com>
2022-04-06 21:05:22 +0200

    Added missing log

---
+++ src/SharpMC.Log/LogManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace SharpMC.Log
+{
+    public static class LogManager
+    {
+        public static ILoggerFactory Factory;
+
+        public static ILogger GetLogger(Type type)
+        {
+            return Factory.CreateLogger(type);
+        }
+    }
+}
