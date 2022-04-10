195bc97d5a2a9767ef88542b42b5ad5023da35f0
xafero <georg.berendt@outlook.com>
2022-04-10 23:34:57 +0200

    Adding server console project

---
+++ src/SharpMC.Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using SharpMC.API.Plugins;
+using SharpMC.Logging;
+using SharpMC.Plugin.Admin;
+using SharpMC.Plugin.Pets;
+using SharpMC.Plugin.Test;
+
+namespace SharpMC.Server
+{
+    internal class Program
+    {
+        private static readonly ILogger Log = LogManager.GetLogger(typeof(Program));
+
+        private static void Main(string[] args)
+        {
+            Log.LogInformation("Initializing...");
+            IServer server = new MinecraftServer();
+            try
+            {
+                server.Start();
+                Console.ReadLine();
+            }
+            finally
+            {
+                server.Stop();
+            }
+            Log.LogInformation("Exited.");
+        }
+
+        // ReSharper disable UnusedMember.Local
+        private static readonly IPlugin[] Dummy =
+        {
+            new MainPlugin(), new TestPlugin(), new PetPlugin()
+        };
+    }
+}
