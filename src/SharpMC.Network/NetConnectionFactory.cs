9d5e7dee9092b378d519e7ac913f9120ecda26c8
xafero <georg.berendt@outlook.com>
2022-04-11 00:25:45 +0200

    Reformat

--- src/SharpMC.Network/NetConnectionFactory.cs
+++ src/SharpMC.Network/NetConnectionFactory.cs
@@ -4,24 +4,25 @@ using SharpMC.Network.Events;
 
 namespace SharpMC.Network
 {
-	public class NetConnectionFactory
-	{
-		public EventHandler<ConnectionCreatedArgs> OnConnectionCreated;
+    public class NetConnectionFactory
+    {
+        public EventHandler<ConnectionCreatedArgs> OnConnectionCreated;
 
-		internal NetConnection CreateConnection(Direction direction, Socket socket, ConnectionConfirmed confirmedAction = null)
-		{
-			var connection = Create(direction, socket, confirmedAction);
+        internal NetConnection CreateConnection(Direction direction, Socket socket,
+            EventHandler<ConnectionConfirmedArgs> confirmedAction = null)
+        {
+            var connection = Create(direction, socket, confirmedAction);
+            if (connection == null)
+                return null;
+            OnConnectionCreated?.Invoke(null, new ConnectionCreatedArgs(connection));
+            return connection;
+        }
 
-			if (connection == null) return null;
-
-			OnConnectionCreated?.Invoke(null, new ConnectionCreatedArgs(connection));
-			return connection;
-		}
-
-		protected virtual NetConnection Create(Direction direction, Socket socket, ConnectionConfirmed confirmedAction = null)
-		{
-			var conn = new NetConnection(direction, socket, confirmedAction);
-			return conn;
-		}
-	}
+        protected virtual NetConnection Create(Direction direction, Socket socket,
+            EventHandler<ConnectionConfirmedArgs> confirmedAction = null)
+        {
+            var conn = new NetConnection(direction, socket, confirmedAction);
+            return conn;
+        }
+    }
 }
