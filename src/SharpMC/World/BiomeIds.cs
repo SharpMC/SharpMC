ca1ae6747b9dc948d2ef47d52e5fdba887ba3b34
xafero <georg.berendt@outlook.com>
2022-04-25 07:01:18 +0200

    Added old biome ids

--- src/SharpMC/World/BiomeIds.cs
+++ src/SharpMC/World/BiomeIds.cs
@@ -5,5 +5,16 @@ namespace SharpMC.World
         None = 0,
 
         Plains = 1
+
+        /*
+        Plains = 0,
+        Forest = 1,
+        Desert = 2,
+        BirchForest = 3,
+        FlowerForest = 4,
+        SunFlowerPlains = 5,
+        Ocean = 8,
+        Beach = 16
+        */
     }
 }
