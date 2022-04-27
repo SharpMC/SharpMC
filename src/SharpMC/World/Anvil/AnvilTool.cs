640e546f829be5f5dd81b74a86c806fbfc947d6d
xafero <georg.berendt@outlook.com>
2022-04-28 01:25:11 +0200

    Added some experimental anvil code

---
+++ src/SharpMC/World/Anvil/AnvilTool.cs
@@ -0,0 +1,22 @@
+﻿namespace SharpMC.World.Anvil
+{
+    internal static class AnvilTool
+    {
+        internal static byte Nibble4(byte[] arr, int index)
+        {
+            return (byte) (index % 2 == 0 ? arr[index / 2] & 0x0F : (arr[index / 2] >> 4) & 0x0F);
+        }
+
+        internal static void SetNibble4(byte[] arr, int index, byte value)
+        {
+            if (index % 2 == 0)
+            {
+                arr[index / 2] = (byte) ((value & 0x0F) | arr[index / 2]);
+            }
+            else
+            {
+                arr[index / 2] = (byte) (((value << 4) & 0xF0) | arr[index / 2]);
+            }
+        }
+    }
+}
